90a1aade919dc04bf2d11b33959c8a0610b0cd8e
Arefin19 <arefinprio31015@gmail.com>
2024-01-04 18:12:04 +0600

    navbar

---
+++ resources/views/navbar.blade.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link href="{{asset('css/navbar.css') }}" rel="stylesheet">
+    <title>Document</title>
+</head>
+<body>
+    <nav>
+        <a href="#" class="logo">Plant Shop</a>
+        <a href="#">Home</a>
+        <a href="#">Catalog</a>
+        <a href="#">About Us</a>
+        <a href="#" class="right">Contact</a>
+    </nav>
+</body>
+</html>
